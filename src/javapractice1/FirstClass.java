aba13e60b6223140543426d9a8e52b1011a08ec3
unknown <ramatobbo5@gmail.com>
2025-04-20 17:57:01 +0300

    Your commit message

---
+++ src/javapractice1/FirstClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javapractice1;
+
+/**
+ *
+ * @author 10User
+ */
+public class FirstClass {
+    
+}
